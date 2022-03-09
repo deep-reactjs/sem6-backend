fddbcbb56fe8959323070578cfd27267d80de743
deep-reactjs <deepsoni704625@gmail.com>
2022-03-10 00:28:50 +0530

    client added to repo

---
+++ client/src/actions/productActions.js
@@ -0,0 +1,67 @@
+import * as api from "../api/index";
+
+import {
+  ADD_NEW_PRODUCT,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT,
+  FETCH_PRODUCTS_BY_USER,
+  FETCH_PRODUCT,
+  START_LOADING,
+  END_LOADING,
+} from "./constants";
+
+export const getProduct = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchProduct(id);
+    dispatch({ type: FETCH_PRODUCT, payload: { product: data } });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProductsByUser = (searchQuery) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const {
+      data: { data },
+    } = await api.fetchProductsByUser(searchQuery);
+    console.log("get products called", data);
+    dispatch({ type: FETCH_PRODUCTS_BY_USER, payload: data });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const createProduct = (product, openSnackbar) => async (dispatch) => {
+  try {
+    const { data } = await api.addProduct(product);
+    dispatch({ type: ADD_NEW_PRODUCT, payload: data });
+    openSnackbar("Customer added successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProduct =
+  (id, product, openSnackbar) => async (dispatch) => {
+    const { data } = await api.updateProduct(id, product);
+    dispatch({ type: UPDATE_PRODUCT, payload: data });
+    openSnackbar("Customer updated successfully");
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+export const deleteProduct = (id, openSnackbar) => async (dispatch) => {
+  try {
+    await api.deleteProduct(id);
+
+    dispatch({ type: DELETE_PRODUCT, payload: id });
+    openSnackbar("Customer deleted successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ client/src/components/Products/ProductList.js
@@ -0,0 +1,96 @@
+/* eslint-disable */
+import React, { useState, useEffect } from "react";
+import Products from "./Products";
+import AddProduct from "./AddProduct";
+import { getProductsByUser } from "../../actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useHistory } from "react-router-dom";
+import NoData from "../svgIcons/NoData";
+import Spinner from "../Spinner/Spinner";
+
+const ProductList = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const [open, setOpen] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const { products } = useSelector((state) => state.products);
+  const isLoading = useSelector((state) => state?.products?.isLoading);
+  // const clients = []
+
+  // useEffect(() => {
+  // }, [currentId, dispatch]);
+
+  //     useEffect(() => {
+  //         dispatch(getClients(1));
+  //         // dispatch(getClientsByUser({userId : user?.result?._id}));
+  //         // dispatch(getClientsByUser({ search :user?.result?._id, tags: tags.join(',') }));
+  //     },[location]
+  // )
+
+  useEffect(() => {
+    dispatch(
+      getProductsByUser({ search: user?.result?._id || user.result.googleId })
+    );
+  }, [location, dispatch]);
+  if (!user) {
+    history.push("/login");
+  }
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          paddingTop: "20px",
+        }}
+      >
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (products?.length === 0) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          paddingTop: "20px",
+          margin: "80px",
+        }}
+      >
+        <NoData />
+        <p style={{ padding: "40px", color: "gray", textAlign: "center" }}>
+          No Products yet. Click the plus icon to add Product
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <AddProduct
+        open={open}
+        setOpen={setOpen}
+        currentId={currentId}
+        setCurrentId={setCurrentId}
+      />
+      <Products
+        open={open}
+        setOpen={setOpen}
+        currentId={currentId}
+        setCurrentId={setCurrentId}
+        products={products}
+      />
+    </div>
+  );
+};
+
+export default ProductList;
